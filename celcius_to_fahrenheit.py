3f9f692e19687b5bfcc9f17457cb8743097583eb
Mehmet Tenekeci <oddium@outlook.com>
2017-12-29 08:17:03 +0300

    init commit

---
+++ celcius_to_fahrenheit.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+Celsius = int(input("Enter a temperature in Celsius: "))
+
+Fahrenheit = 9.0/5.0 * Celsius + 32
+
+print ("Temperature:", Celsius, "Celsius = ", Fahrenheit, " F")
